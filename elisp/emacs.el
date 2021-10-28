7226dd23b1afb99b230776a06eff3f722a328b4f
QP <qp@example.com>
2021-10-29 01:13:29 +0200

    importing unused ido code

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -19,3 +19,19 @@
 	 (progn (delete-forward-char 1)
 		(insert-char ?ū)))
 	(t (forward-char))))
+
+
+;; ;; https://www.masteringemacs.org/article/introduction-to-ido-mode
+;; (setq ido-enable-flex-matching t)
+;; (setq ido-everywhere t)
+;; (ido-mode 1)
+
+;; (setq ido-use-filename-at-point 'guess)
+;; (setq ido-create-new-buffer 'always)
+
+;; ;; Don't ask for confirmation on creating a new buffer.
+;; ;; From a comment on:
+;; ;; https://www.masteringemacs.org/article/introduction-to-ido-mode
+;; (defadvice ido-switch-buffer (around no-confirmation activate)
+;;   (let ((confirm-nonexistent-file-or-buffer nil))
+;;     ad-do-it))
