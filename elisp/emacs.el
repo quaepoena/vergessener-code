88fb5e789f1baf6a1914ddeb404bc6be62fe54f1
QP <qp@example.com>
2023-06-08 10:27:42 +0200

    adding the `count-overdue` function

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -14,3 +14,31 @@
 (defadvice ido-switch-buffer (around no-confirmation activate)
   (let ((confirm-nonexistent-file-or-buffer nil))
     ad-do-it))
+
+;; No longer in use, but I refer back to it now and again.
+(defun count-overdue (start end)
+  """Format the overdue values from Org Agenda for summing and insertion into the kill ring. """
+  (interactive "r")
+
+  (let ((oldbuf (current-buffer)))
+    (with-temp-buffer
+      (insert-buffer-substring oldbuf start end)
+
+      (goto-char (point-min))
+      (while (re-search-forward ".*?\\([[:digit:]]+\\).*" nil t)
+	(replace-match "\\1"))
+
+      (goto-char (point-min))
+      (subst-char-in-region (point-min) (point-max) ?\n ?\s)
+
+      (fixup-whitespace)
+      (insert "(+ ")
+      (delete-trailing-whitespace)
+
+      (goto-char (point-max))
+      (insert ")")
+
+      (kill-new
+       (number-to-string
+	(eval (car (read-from-string
+		    (buffer-substring-no-properties (point-min) (point-max))))))))))
