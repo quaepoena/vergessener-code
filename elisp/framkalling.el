ed500259a6bf70719003a66745c8a4a1de392915
QP <qp@example.com>
2024-05-02 11:32:55 +0200

    leggja til framkalling.el

---
+++ elisp/framkalling.el
@@ -0,0 +1,102 @@
+(defun framkalling (mappe1 mappe2)
+  (declare-function mønstermåta "framkalling" (fil avløysar))
+  (declare-function filnamn "framkalling" (fil))
+  (declare-function få-hjelpetekst "framkalling" (x))
+  (declare-function visa-fil "framkalling" (fil))
+
+  (defun mønstermåta (fil avløysar)
+    "Mønstermåta filnamnet."
+    (replace-regexp-in-string (car avløysar) (cdr avløysar) fil))
+
+  (defun filnamn (fil)
+    "Returnera alt etter den siste \"/\"-en av FIL."
+    (when (string-match (rx bol
+			    (zero-or-more any)
+			    "/"
+			    (group (one-or-more any)
+				   "."
+				   (one-or-more alnum))
+			    eol)
+			fil)
+      (match-string 1 fil)))
+
+  (defun visa-fil (fil)
+    (find-file-other-window fil)
+    (other-window 1))
+
+  (defun få-hjelpetekst (x)
+    "Hjelpeteksta som viser um eit bilete vert med."
+    (if x
+	"Dette biletet vert med."
+      "Dette biletet vert IKKJE med."))
+
+  (let* ((case-fold-search t)
+	 (filtype-rx (rx (or "jpg" "jpeg" "png") eol))
+	 (tmp-filar (directory-files-recursively mappe1 filtype-rx nil t))
+	 (filar (mapcar (lambda (x) `(,x . nil)) tmp-filar))
+	 (n (length filar))
+	 (pos 0)
+	 (avløysarpar (list (cons (rx "jpeg" eol) "jpg"))))
+
+    (catch 'avslutta
+      (while t
+	(catch 'start
+	  (when (< pos 0)
+	    (message "Du er på det fyrste biletet.")
+	    (sleep-for 2)
+	    (throw 'start (setf pos (1+ pos))))
+
+	  (when (>= pos n)
+	    (message "Du er på det siste biletet.")
+	    (sleep-for 2)
+	    (throw 'start (setf pos (1- n))))
+
+	  (visa-fil (car (elt filar pos)))
+
+	  (let ((svar (completing-read
+		       (concat (få-hjelpetekst (cdr (elt filar pos)))
+			       "\n\n"
+			       "Behalda? (j/n)\n"
+			       "Førre/Próximo? (f/p)\n"
+			       "Gå til Starten/Enden (S/E)\n"
+			       "Avslutta (a): ")
+		       '("j" "n" "f" "p" "a" "S" "E" "") nil t)))
+
+	    (cond ((string-equal svar "j") (setf (cdr (elt filar pos)) t
+						 pos (1+ pos)))
+		  ((string-equal svar "n") (setf (cdr (elt filar pos)) nil
+						 pos (1+ pos)))
+		  ((string-equal svar "f") (setf pos (1- pos)))
+		  ((or (string-equal svar "p") (string-empty-p svar))
+		   (setf pos (1+ pos)))
+		  ((string-equal svar "S") (setf pos 0))
+		  ((string-equal svar "E") (setf pos (1- n)))
+		  ((string-equal svar "a") (throw 'avslutta t)))))))
+
+    (dolist (fil filar)
+      (when (cdr fil)
+	(copy-file (car fil) (concat mappe2 (seq-reduce
+					     #'mønstermåta
+					     avløysarpar
+					     (downcase (filnamn (car fil)))))
+		   t t)))))
+
+(defun framkalling-få-filtype (x)
+  (when (string-match (rx bol
+			  (zero-or-more any)
+			  "/"
+			  (one-or-more any)
+			  "."
+			  (group (one-or-more alnum))
+			  eol)
+		      x)
+    (match-string 1 x)))
+
+(defun framkalling-få-filtypane (dir)
+  (delete-dups
+   (cl-loop for fil being the elements of (directory-files-recursively dir "" nil t)
+	    collect (framkalling-få-filtype fil))))
+
+;; Skapa destinasjonsmappa.
+;; HEIC-filar?
+;; Bilete utan filtype?
