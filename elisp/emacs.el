21b9db4085a0e09f39b537bfa72f4ee0086e82d7
QP <quaepoena@example.com>
2024-08-21 13:11:37 +0200

    trivial ido code

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -1,13 +1,5 @@
 ;; TODO: Remove trivial code, document anything that remains.
 
-;; https://www.masteringemacs.org/article/introduction-to-ido-mode
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-(setq ido-use-filename-at-point 'guess)
-(setq ido-create-new-buffer 'always)
-
 ;; Don't ask for confirmation on creating a new buffer.
 ;; From a comment on:
 ;; https://www.masteringemacs.org/article/introduction-to-ido-mode
